4cb6d8755dfe1ee4bda900a9c28ca131a303703b
Mikalai Zoryn <mikalaizoryn@gmail.com>
2026-07-16 21:41:59 -0700

    Add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
 export default defineConfig({
-  plugins: [react()],
-  base: "/quiz-my-character/",
+    plugins: [react()],
+    base: "/quiz-my-character/",
 });
